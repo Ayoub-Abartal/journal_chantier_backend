28e44eae619fb7d088939c553774a297e3648cc2
ayoub-abartal <abaayoub8@gmail.com>
2024-08-24 19:23:08 +0100

    Fixing the relationships between journal and other entities

--- src/main/java/com/abartal/Journal_chantier/entities/HumanResource.java
+++ src/main/java/com/abartal/Journal_chantier/entities/HumanResource.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="human_ressource")
+@Table(name="human_resource")
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/com/abartal/Journal_chantier/entities/Journal.java
+++ src/main/java/com/abartal/Journal_chantier/entities/Journal.java
@@ -28,24 +28,24 @@ public class Journal extends BaseEntity{
     @JoinColumn(name="project_id")
     private Project project;
 
-    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "journal", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Inventory> inventories;
 
-    @OneToMany(mappedBy="visitor", cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy="journal", cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Visitor> visitors;
 
-    @OneToMany(mappedBy = "observation", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "journal", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Observation> observations;
 
-    @OneToMany(mappedBy="work_time", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy="journal", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<WorkTime> workTimes;
 
-    @OneToMany(mappedBy = "incidence",cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "journal",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Incidence> incidences;
 
-    @OneToMany(mappedBy = "human_resource",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "journal",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<HumanResource> humanResources;
 
-    @OneToMany(mappedBy = "signature",cascade = CascadeType.ALL,orphanRemoval = true)
+    @OneToMany(mappedBy = "journal",cascade = CascadeType.ALL,orphanRemoval = true)
     private List<Signature> signatures;
 }
